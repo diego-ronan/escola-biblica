43cd32fc2ff85b4a0eea51fc8b4eeb9fa22da28d
Diego Ronan Félix <diegoronanfelix@gmail.com>
2024-05-01 18:32:15 -0300

    Melhorias no metodo attendanceCreate

--- app/Livewire/AttendanceCreate.php
+++ app/Livewire/AttendanceCreate.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Attendance;
 use App\Models\Student;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class AttendanceCreate extends Component
@@ -12,17 +13,15 @@ class AttendanceCreate extends Component
     public $student_name = [];
     public $attendance_status = [];
     public $classroom_id = [];
-
+    
     public function save()
     {
-        $attendanceCount = count($this->attendance_status);
-
-        for ($i = 0; $i < $attendanceCount; $i++) {
+        foreach ($this->attendance_status as $key => $status) {
             Attendance::create([
                 'date' => date('Y-m-d', strtotime($this->date)),
-                'student_name' => $this->student_name[$i],
-                'attendance_status' => $this->attendance_status[$i],
-                'classroom_id' => $this->classroom_id[$i]
+                'student_name' => $this->student_name[$key],
+                'attendance_status' => $status,
+                'classroom_id' => $this->classroom_id[$key]
             ]);
         }
     }
